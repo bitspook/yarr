6a910e99220caf34b73af8a492731efeb23641f6
Charanjit Singh <ckhabra007@gmail.com>
2015-07-23 00:58:01 +0530

    Hello Virtual-DOM
    Incrementing counter with setTimeout

--- src/index.js
+++ src/index.js
@@ -1,66 +1,30 @@
-import {Observable} from 'rx';
-import {ajax} from 'jQuery';
+import {h} from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
+import diff from 'virtual-dom/diff';
+import patch from 'virtual-dom/patch';
 
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let render = (count) => <h1 className='hello-world'>Hello World {count + ''}</h1>;
+//we did a `count = ''` above because `h` can't handle integers in this case, it need strings
 
-//convert a js array to Observable
-let nums_ = Observable.from(nums);
+let count = 0;
 
-//Let's filter out odd numbers from `nums_` and find evens_.
-let evens_ = nums_
-      .filter(n => n % 2 === 0);
+//render gives us our whole view, only one h1 in our case
+let view = render(count);
+//in virtual DOM, there has to be a single ultimate parent to hold all vNodes. Let's call it rootNode
+let rootNode = createElement(view)
 
-//Observable don't execute any code in the chain until and unless there is at
-//least one active subscription to the Observable.
-evens_
-  .subscribe(x => console.log('Even: ', x));
+document.body.appendChild(rootNode);
 
+setInterval(function() {
+  count ++;
 
-//Promises in Observables
-let urls = [
-  'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=test'
-];
+  //we create new view with new state
+  let newView = render(count);
+  //now let's diff the old with the new view and create patches
+  let patches = diff(view, newView);
+  //let's create new rootNode by patching the old rootNode with the patches we got from diffing
+  rootNode = patch(rootNode, patches);
+  //and change our saved view for diffing next time we need to update DOM
+  view = newView;
 
-Observable
-  .from(urls)
-  .flatMap(url => ajax({url, dataType: 'jsonp'}))
-  .subscribe(
-    res => console.log('Response: ', res),
-    err => console.error('Error while fetching:', err),
-    () => console.log('Done with all requests')
-  );
-
-//Observable.merge
-Observable
-  .interval(1000)
-  .merge(
-    Observable.interval(2000)
-  )
-//  .subscribe(x => console.log('Merged: ', x));
-
-//Observable.combineLatest
-let resize_ = Observable.fromEvent(window, 'resize');
-let click_ = Observable.fromEvent(window, 'click');
-
-Observable
-  .combineLatest(
-    resize_.startWith(null),
-    click_.startWith(null),
-    (rE, cE) => {
-      return 'Lol!';
-    }
-  )
-  .subscribe(
-    x => console.log('Window resized or clicked', x)
-  );
-
-//Observable.prototype.zip
-let evenObs_ = Observable.from([0, 2, 4, 6, 8, 10]);
-let oddObs_ = Observable.from([1, 3, 5, 7, 9]);
-
-evenObs_
-  .zip(
-    oddObs_,
-    (even, odd) => even + odd
-  )
-  .subscribe(x => console.log('Even+Odd: ', x));
+}, 1000);
