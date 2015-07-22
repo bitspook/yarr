492653461f26fdd0b6b64ca7c6b425fa9e748de0
Charanjit Singh <ckhabra007@gmail.com>
2015-07-22 19:03:49 +0530

    Add live-reload to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var getEntryPoints = function(sources) {
+  if(process.env.NODE_ENV !== 'production')
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+
+  return sources;
+};
+
 module.exports = {
   entry: {
-    app: ['./src/index.js']
+    app: getEntryPoints(['./src/index.js'])
   },
   output: {
     filename: 'dist/[name].js'
