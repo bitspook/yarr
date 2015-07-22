724968c8a360f332a4a12de11c1ae445162f4d14
Charanjit Singh <ckhabra007@gmail.com>
2015-07-23 01:14:05 +0530

    Virtual-dom update with Observable

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
+import {Observable} from 'rx';
+
 import {h} from 'virtual-dom';
 import createElement from 'virtual-dom/create-element';
 import diff from 'virtual-dom/diff';
 import patch from 'virtual-dom/patch';
 
+
 let render = (count) => <h1 className='hello-world'>Hello World {count + ''}</h1>;
 //we did a `count = ''` above because `h` can't handle integers in this case, it need strings
 
-let count = 0;
-
-//render gives us our whole view, only one h1 in our case
-let view = render(count);
-//in virtual DOM, there has to be a single ultimate parent to hold all vNodes. Let's call it rootNode
-let rootNode = createElement(view)
-
+let view = render(0);
+let rootNode = createElement(view);
 document.body.appendChild(rootNode);
 
-setInterval(function() {
-  count ++;
-
-  //we create new view with new state
-  let newView = render(count);
-  //now let's diff the old with the new view and create patches
-  let patches = diff(view, newView);
-  //let's create new rootNode by patching the old rootNode with the patches we got from diffing
-  rootNode = patch(rootNode, patches);
-  //and change our saved view for diffing next time we need to update DOM
-  view = newView;
-
-}, 1000);
+Observable
+  .interval(1000) //replace the steTimeout and `count` state variable. Interval will give us an increment-ing number every 1000 milliseconds
+  .map(n => render(n + 1))
+  .subscribe(
+    newView => {
+      let patches = diff(view, newView);
+      rootNode = patch(rootNode, patches);
+      view = newView;
+    }
+  );
