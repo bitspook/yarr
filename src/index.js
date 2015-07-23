38e6604297f382e652daeb30e03dbbe12b5826b1
Charanjit Singh <ckhabra007@gmail.com>
2015-07-23 17:50:57 +0530

    Implement components and renderer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import {Observable} from 'rx';
+import mainView_ from './components/main';
+import render from './renderer';
 
-import {h} from 'virtual-dom';
-import createElement from 'virtual-dom/create-element';
-import diff from 'virtual-dom/diff';
-import patch from 'virtual-dom/patch';
+let view_ = mainView_();
 
-
-let render = (count) => <h1 className='hello-world'>Hello World {count + ''}</h1>;
-//we did a `count = ''` above because `h` can't handle integers in this case, it need strings
-
-let view = render(0);
-let rootNode = createElement(view);
-document.body.appendChild(rootNode);
-
-Observable
-  .interval(1000) //replace the steTimeout and `count` state variable. Interval will give us an increment-ing number every 1000 milliseconds
-  .map(n => render(n + 1))
-  .subscribe(
-    newView => {
-      let patches = diff(view, newView);
-      rootNode = patch(rootNode, patches);
-      view = newView;
-    }
-  );
+render(view_, document.getElementById('app'));
